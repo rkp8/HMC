8c6d279a47e420bcbe6f9e31c49d0850544aaabc
rkp8 <60204834+rkp8@users.noreply.github.com>
2020-05-29 18:31:15 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,20 @@
+package game_theory_bimatrix;
+
+public interface Game {
+    /**
+ * @param myAction action played by hero
+ * @param otherAction action played by villain
+ * @param player what is player number for player that played myAction * @return utility for the player of interest
+ */
+int utility(int myAction, int otherAction, int player);
+    /**
+     * Provide a description of an action
+     *
+     * @param actionIndex the integer representing the action * @return the description of the action
+     */
+    String actionAsString(int actionIndex);
+
+    String getType();
+
+}
+
